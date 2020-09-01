ee343b17a45b7d6bb5f2209d2ed34ed9c52e0d1d
Chaim Bryski <luzybry94@gmail.com>
2020-09-01 20:02:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,4 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-puts doc.css("inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text
+doc.css("inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
