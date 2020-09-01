445a533a65459d640a3a5f8884bfbea89022005c
Chaim Bryski <luzybry94@gmail.com>
2020-09-01 20:06:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,4 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-puts doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586")
+puts doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586").text
